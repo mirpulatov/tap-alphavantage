07fb974fdb32477bc221e6c3577090780995db02
Исломбек Мирпулатов <i.mirpulatov@OFB.local>
2020-08-28 12:02:07 +0500

    added schemas and cli interface

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
 # tap-alphavantage
-soon
+
+
+  <p align="center">
+</p>
+
+<!-- TABLE OF CONTENTS -->
+## Table of Contents
+
+* [About the Project](#about-the-project)
+* [Getting Started](#getting-started)
+  * [Prerequisites](#prerequisites)
+  * [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Contact](#contact)
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+Buliding TAP for Alpha Vantage API using Singer.io Python
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+To get a local copy up and running follow these simple steps.
+
+
+
+### Prerequisites
+
+This is an example of how to list things you need to use the software and how to install them.
+* Python 3.8
+* Singer.io
+
+
+### Installation
+
+1. Clone the repo
+```sh
+git clone https://github.com/mirpulatov/tap-alphavantage.git
+```
+2. Install Python packages
+```sh
+pip install -r requirements.txt
+```
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+Geting json FUNDAMENTAL DATA and convert to csv
+
+Company overview:
+```sh
+python main.py company-overview | target csv
+```
+Income statement:
+```sh
+python main.py income-statement | target csv
+```
+Balance Sheet:
+```sh
+python main.py balance-sheet | target csv
+```
+Cash Flow:
+```sh
+python main.py cash-flow | target csv
+```
+
+<!-- CONTACT -->
+## Contact
+
+Mirpulatov Islombek - [imirpulatov@icloud.com](imirpulatov@icloud.com) - email
+
+Tap AlphaVantage: [https://github.com/mirpulatov/tap-alphavantage](https://github.com/mirpulatov/tap-alphavantage)
+
+<!-- COMMENTS -->
+## Comments
+
+It's bad that singer.io has poor documentation. Also, I did not completely transfer all the data tabs, since this 
+is more mechanical work.
+
+I found a Cookicutter - a template for organizing a project, but the first time I decided to do it manually.
+
--- main.py
+++ main.py
@@ -6,29 +6,47 @@ import sys
 # Fundamental data urls
 URL_COMPANY_OVERVIEW = 'https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=JEPP7M48FZLYH6C0'
 URL_INCOME_STATEMENT = 'https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=IBM&apikey=JEPP7M48FZLYH6C0'
+URL_BALANCE_SHEET = 'https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol=IBM&apikey=JEPP7M48FZLYH6C0'
+URL_CASH_FLOW = 'https://www.alphavantage.co/query?function=CASH_FLOW&symbol=IBM&apikey=JEPP7M48FZLYH6C0'
 
+# Reading CLI arg, key of
 argument = sys.argv[1]
 
 
 def read_schema(file_name):
+    """
+    Reading schema data from json file
+    :param file_name: file name
+    :return: data from json
+    """
     with open(file_name) as json_file:
         data = json.load(json_file)
         return data
 
 
-def singer_data(url, singer_schema):
+def singer_data(url, singer_schema, stream):
+    """
+    Request data from url, ETL data
+    :param url: URL to data
+    :param singer_schema: JSON shema
+    :param stream: name of stream
+    :return: nothing
+    """
     data = requests.get(url)
-    singer.write_schema(schema=singer_schema, stream_name='my_stream', key_properties=[""])
-    singer.write_record(stream_name='my_stream', record=data.json())
+    singer.write_schema(schema=singer_schema, stream_name=stream, key_properties=[""])
+    singer.write_record(stream_name=stream, record=data.json())
 
 
+# choosing the type of data
 if argument == 'company-overview':
     schema = read_schema('schemas/company_overview.json')
-    singer_data(URL_COMPANY_OVERVIEW, schema)
+    singer_data(URL_COMPANY_OVERVIEW, schema, 'company_overview')
 elif argument == 'income-statement':
     schema = read_schema('schemas/income_statement.json')
-    singer_data(URL_INCOME_STATEMENT, schema)
-
-
-
-
+    singer_data(URL_INCOME_STATEMENT, schema, 'income_statement')
+elif argument == 'balance-sheet':
+    schema = read_schema('schemas/balance_sheet.json')
+    singer_data(URL_BALANCE_SHEET, schema, 'balance_sheet')
+elif argument == 'cash-flow':
+    schema = read_schema('schemas/cash_flow.json')
+    singer_data(URL_CASH_FLOW, schema, 'cash_flow')
